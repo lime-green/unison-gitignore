ccb06c91a9c1f8d3056248522c58323dd8f6dc2d
Joshua Doncaster-Marsiglio <josh.doncastermarsiglio@tophatmonocle.com>
2020-06-28 14:56:35 -0400

    alias unison

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,6 +2,8 @@ import subprocess
 
 
 def test_basic_command_completes_successully():
+    # Since unison won't exist on CI
+    subprocess.run(["alias", "unison=echo"])
     p = subprocess.run(
         ["unison_gitignore", "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
